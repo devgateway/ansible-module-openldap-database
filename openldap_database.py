bc2686087434903aa10e7e056a2bcd73beb54db3
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-04-05 16:10:09 -0400

    Initial commit

---
+++ openldap_database.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+# Copyright 2018, Development Gateway <info@developmentgateway.org>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
